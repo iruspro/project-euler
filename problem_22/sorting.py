71ae60cbe6711ee9330424bf60c34225a71c2243
iruspro <iruspro11@mail.ru>
2020-03-08 16:55:13 +0300

    The names from the file are added to the list and a quick sorting algorithm is implemented

---
+++ problem_22/sorting.py
@@ -0,0 +1,26 @@
+import re
+
+
+def quick_sort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+path = 'p022_names.txt'
+with open(path, encoding='utf-8') as file:
+    text = file.readline()
+
+list_names = list()
+while True:
+    match = re.search(r'\"\w*\"', text)
+    if match:
+        list_names.append(text[match.start() + 1:match.end() - 1])
+        text = text[match.end():]
+    else:
+        break
+list_names = quick_sort(list_names)
